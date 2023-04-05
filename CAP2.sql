695a56c22bd363c4ed2b08e4c395abcfe82d0047
Sabryna Angelo <sabrynadeangelo@gmail.com>
2023-04-05 09:52:36 -0300

    Executando comandos e add boas práticas

--- CAP2.sql
+++ CAP2.sql
@@ -1,70 +1,70 @@
-SELECT COD_ALUNO, NOME, CIDADE
-FROM TALUNO;
+SELECT cod_aluno, nome, cidade
+FROM taluno;
 
 
-SELECT COD_ALUNO AS "Codigo", NOME AS "Nome do Aluno"
-FROM TALUNO;
+SELECT cod_aluno AS "Codigo", nome AS "Nome do Aluno"
+FROM taluno;
 
 --Distinct retira linhas duplicadas
 --Todas as colunas tem que ser iguais
 
-SELECT CIDADE FROM TALUNO;
+SELECT cidade FROM taluno;
 
 
-SELECT DISTINCT CIDADE FROM TALUNO;
+SELECT DISTINCT cidade FROM taluno;
 
 
 --Nao agrupa pois cod_aluno e diferente
 --Para cada linha
-SELECT DISTINCT CIDADE, COD_ALUNO
-FROM TALUNO
-ORDER BY CIDADE;
+SELECT DISTINCT cidade, cod_aluno
+FROM taluno
+ORDER BY cidade;
 
 
 -----CALCULOS-----
 
-SELECT NOME AS CURSO,
-       VALOR,
-       VALOR/CARGA_HORARIA,
-       Round(VALOR/CARGA_HORARIA,2) AS VALOR_HORA
-FROM TCURSO
-ORDER BY VALOR_HORA;
+SELECT nome AS curso,
+       valor,
+       valor/carga_horaria,
+       Round(valor/carga_horaria,2) AS valor_hora
+FROM tcurso
+ORDER BY valor_hora;
 
 
 --Apelido de coluna so funciona em ORDER BY
 
-SELECT * FROM TCONTRATO;
+SELECT * FROM tcontrato;
 
 
-UPDATE TCONTRATO SET
-DESCONTO = NULL
-WHERE COD_CONTRATO = 4;
+UPDATE tcontrato SET
+desconto = NULL
+WHERE cod_contrato = 4;
 
 --Calculo com colunas = NULL
 --Resultado = NULL
 --Lembrando que qualquer coluna que estver NULL ira anular o calculo
 
-SELECT COD_CONTRATO,
-       TOTAL,
-       DESCONTO,
-       TOTAL+DESCONTO
-FROM TCONTRATO;
+SELECT cod_contrato,
+       total,
+       desconto,
+       total + desconto
+FROM tcontrato;
 
 --Tornando o NULL em 0
 
-SELECT COD_CONTRATO,
-       DESCONTO,
-       Nvl(DESCONTO,0),
-       TOTAL,
-       TOTAL + Nvl(DESCONTO,0) AS TOTAL_MAIS_DESCONTO
-FROM TCONTRATO;
+SELECT cod_contrato,
+       desconto,
+       Nvl(desconto,0),
+       total,
+       total + Nvl(desconto,0) AS total_mais_desconto
+FROM tcontrato;
 
 
 --Exemplo de concatenacao
 
-SELECT COD_ALUNO || ' - ' || NOME || ' // ' || CIDADE AS ALUNO
-FROM TALUNO
-ORDER BY COD_ALUNO;
+SELECT cod_aluno || ' - ' || nome || ' // ' || cidade AS aluno
+FROM taluno
+ORDER BY cod_aluno;
 
 COMMIT;
 
