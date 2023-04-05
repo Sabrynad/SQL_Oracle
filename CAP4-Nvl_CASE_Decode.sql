34c12e39c51f497b77142b0480cb7468a848bcc6
Sabryna Angelo <sabrynadeangelo@gmail.com>
2023-04-05 10:40:09 -0300

    Funções básicas Nvl_Case_Decode. Add boas práticas

--- CAP4-Nvl_CASE_Decode.sql
+++ CAP4-Nvl_CASE_Decode.sql
@@ -1,55 +1,55 @@
 --NVL E NVL2
 
-SELECT * FROM TCONTRATO;
+SELECT * FROM tcontrato;
 
-SELECT Total,
-       Desconto,
-       Desconto+Total,
-       Nvl(Desconto,0),
-       Nvl(Desconto,0) + TOTAL,
-       Nvl2(DESCONTO, TOTAL, -1)
-FROM TCONTRATO;
+SELECT total,
+       desconto,
+       desconto + total,
+       Nvl(desconto,0),
+       Nvl(desconto,0) + total,
+       Nvl2(desconto, total, -1)
+FROM tcontrato;
 
-SELECT * FROM TALUNO;
+SELECT * FROM taluno;
 
-UPDATE TALUNO SET NOME = NULL WHERE COD_ALUNO = 46;
+UPDATE taluno SET nome = NULL WHERE cod_aluno = 46;
 
-SELECT Cod_Aluno, Nvl(Nome, 'SEM NOME') FROM TALUNO;
+SELECT cod_aluno, Nvl(Nome, 'SEM NOME') FROM taluno;
 
-SELECT * FROM TALUNO;
-
-
-UPDATE TAluno SET
-Estado = 'RJ'
-WHERE Cod_Aluno = 2;
+SELECT * FROM taluno;
 
 
-UPDATE TAluno SET
-Estado = 'SP'
-WHERE Cod_Aluno = 2;
+UPDATE taluno SET
+estado = 'RJ'
+WHERE cod_aluno = 2;
+
+
+UPDATE taluno SET
+estado = 'SP'
+WHERE cod_aluno = 2;
 
 
 --Case
-SELECT NOME, Estado,
+SELECT nome, estado,
     CASE
-      WHEN Estado = 'SP' THEN 'PAULISTA'
-      WHEN Estado = 'RJ' THEN 'CARIOCA'
-      WHEN Estado = 'BA' AND SALARIO > 500 THEN 'BAIANO'
+      WHEN estado = 'SP' THEN 'PAULISTA'
+      WHEN estado = 'RJ' THEN 'CARIOCA'
+      WHEN estado = 'BA' AND salario > 500 THEN 'BAIANO'
       ELSE 'OUTROS'
-     END AS Apelido
-FROM TALUNO;
+     END AS apelido
+FROM taluno;
 
 
 --
-SELECT SYSDATE AS DATA FROM DUAL
+SELECT SYSDATE AS data FROM dual
 
 --
-SELECT NOME, ESTADO,
-       Decode(ESTADO,'SP','PAULISTA',
+SELECT nome, estado,
+       Decode(estado,'SP','PAULISTA',
                      'RJ','CARIOCA',
                      'BA','BAIANO',
-                          'OUTROS') AS APELIDO
-FROM TALUNO
+                          'OUTROS') AS apelido
+FROM taluno
 
 COMMIT;
 
