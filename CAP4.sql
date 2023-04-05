598c09ed13abda1fe95f25816d8da2637baabf33
Sabryna Angelo <sabrynadeangelo@gmail.com>
2023-04-05 10:21:43 -0300

    Funções básicas e add boas práticas

--- CAP4.sql
+++ CAP4.sql
@@ -1,98 +1,98 @@
 --
-SELECT * FROM TALUNO;
+SELECT * FROM taluno;
 
 
 -- Concat -> juntar dois valores
-SELECT Concat(COD_ALUNO,NOME) FROM TALUNO;
+SELECT Concat(cod_aluno,nome) FROM taluno;
 
 
 --Outra opcao de juncao de valores
-SELECT COD_ALUNO||'-'||NOME FROM TALUNO;
+SELECT cod_aluno||'-'||nome FROM taluno;
 
 
 --IntCap -> tarnsforma a primeira letra de cada palavra em maiuscila e o resto em minuscula
-SELECT nome, InitCap(NOME) FROM TALUNO;
+SELECT nome, InitCap(nome) FROM taluno;
 
 
 --InStr -> retorna dentro do caracter a posicao da letr 'R' por examplo
-SELECT nome, InStr(NOME,'R') FROM TALUNO;
+SELECT nome, InStr(nome,'R') FROM taluno;
 
 
 -- Length ->conta quantos caracteres tem na coluna
-SELECT nome, Length(NOME) FROM TALUNO;
+SELECT nome, Length(nome) FROM taluno;
 
 
 --Lower -> transforma a tabela em minuscula
-SELECT nome, Lower(NOME) FROM TALUNO;
+SELECT nome, Lower(nome) FROM taluno;
 
 
 --Upper -> transforma a tabela em maiuscula
-SELECT nome, Upper(NOME) FROM TALUNO;
+SELECT nome, Upper(nome) FROM taluno;
 
 --
-SELECT InitCap('SABRYNA DE ANGELO') FROM TALUNO;
+SELECT InitCap('SABRYNA DE ANGELO') FROM taluno;
 
 
 --Left -> esquerda
-SELECT cod_aluno, LPad(COD_ALUNO,5,'0') FROM TALUNO;
+SELECT cod_aluno, LPad(cod_aluno,5,'0') FROM taluno;
 
 
 --Right -> direita
-SELECT nome, salario, RPad(SALARIO,8,'0') FROM TALUNO;
+SELECT nome, salario, RPad(salario,8,'0') FROM taluno;
 
 --
-SELECT nome, RPad(NOME,10,'$') FROM TALUNO;
+SELECT nome, RPad(nome,10,'$') FROM taluno;
 
 
 -- SubStr -> copia parte de um texto
 -- substr (campo/texto, posicao, quantidade de caracter)
-SELECT nome, SubStr(NOME,1,3) FROM TALUNO;
+SELECT nome, SubStr(nome,1,3) FROM taluno;
 
 --
-SELECT nome, SubStr(NOME,1,1) FROM TALUNO;
+SELECT nome, SubStr(nome,1,1) FROM taluno;
 
 --
-SELECT nome, SubStr(NOME,3,1) FROM TALUNO;
+SELECT nome, SubStr(nome,3,1) FROM TALUNO;
 
 --
-SELECT REPLACE(Upper(NOME),'R','$') FROM TALUNO;
+SELECT REPLACE(Upper(nome),'R','$') FROM taluno;
 
 --
 
-SELECT nome, SubStr(NOME,Length(nome),1) FROM TALUNO;
+SELECT nome, SubStr(nome,Length(nome),1) FROM taluno;
 
 --
-SELECT nome, SubStr(NOME,Length(nome)-1,2) FROM TALUNO;
+SELECT nome, SubStr(nome,Length(nome)-1,2) FROM TALUNO;
 
 --
-SELECT nome, SubStr(NOME, 3, Length(nome)-3) FROM TALUNO;
+SELECT nome, SubStr(nome, 3, Length(nome)-3) FROM taluno;
 
 COMMIT;
 
-SELECT * FROM TALUNO
-WHERE LOWER(NOME) = 'marta';
+SELECT * FROM taluno
+WHERE LOWER(nome) = 'marta';
 
-SELECT * FROM TALUNO
-WHERE Upper(NOME) = 'TONY';
+SELECT * FROM taluno
+WHERE Upper(nome) = 'TONY';
 
-SELECT * FROM TALUNO
-UNOWHERE Upper(SubStr(CIDADE,1,3)) = 'SAO';
+SELECT * FROM taluno
+WHERE Upper(SubStr(cidade,1,3)) = 'SAO';
 
 
-UPDATE TALUNO SET
-SALARIO = 688.50
-WHERE COD_ALUNO = 1;
+UPDATE taluno SET
+salario = 688.50
+WHERE cod_aluno = 1;
 
 
 
 
 SELECT
-  SALARIO,
-  REPLACE(SALARIO, ',' , ''),
-  RPad(SALARIO, 10, '0'),  --Zeros a direita ate 10 casas
-  LPad(SALARIO, 10, '0'),  --Zeros a esquerda ate 10 casas
-  LPad(REPLACE(SALARIO, ',',''),10,'0')
-FROM TALUNO;
+  salario,
+  REPLACE(salario, ',' , ''),
+  RPad(salario, 10, '0'),  --Zeros a direita ate 10 casas
+  LPad(salario, 10, '0'),  --Zeros a esquerda ate 10 casas
+  LPad(REPLACE(salario, ',',''),10,'0')
+FROM taluno;
 
 
 COMMIT;
