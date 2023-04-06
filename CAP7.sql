ae174cd18c4220e18c3f6070bd1ea7bf0cb64a56
Sabryna Angelo <sabrynadeangelo@gmail.com>
2023-04-06 14:52:51 -0300

    Sub-Consultas e add boas práticas

--- CAP7.sql
+++ CAP7.sql
@@ -1,101 +1,101 @@
-SELECT COD_CONTRATO, DATA, TOTAL
-FROM TCONTRATO
-WHERE TOTAL >=
-  (SELECT VALOR FROM TCURSO
-   WHERE COD_CURSO = 2);
+SELECT cod_contrato, data, total
+FROM tcontrato
+WHERE total >=
+  (SELECT valor FROM tcurso
+   WHERE cod_curso = 2);
 
 
 --Errado(so pode retornar 1 linha na subconsulta)
-SELECT COD_CONTRATO, DATA, TOTAL
-FROM TCONTRATO
-WHERE TOTAL >=
-  (SELECT VALOR FROM TCURSO
-   WHERE VALOR > 500);
+SELECT cod_contrato, data, total
+FROM tcontrato
+WHERE total >=
+  (SELECT valor FROM tcurso
+   WHERE valor > 500);
 
-SELECT * FROM TALUNO;
+SELECT * FROM taluno;
 
 --Todos os alunos da mesma cidade do Aluno 1
 --menos o Aluno 1
-SELECT COD_ALUNO, NOME, CIDADE
-FROM TALUNO
-WHERE CIDADE = (SELECT CIDADE FROM TALUNO
-                WHERE COD_ALUNO = 1)
-AND COD_ALUNO <> 1;
+SELECT cod_aluno, nome, cidade
+FROM taluno
+WHERE cidade = (SELECT cidade FROM taluno
+                WHERE cod_aluno = 1)
+AND cod_aluno <> 1;
 
 
 --Todos os alunos da mesma cidade e estados do aluno 1
 --Menos o aluno 1
-SELECT COD_ALUNO, NOME, CIDADE, ESTADO
-FROM TALUNO
-WHERE (CIDADE,ESTADO) =
-         (SELECT CIDADE, ESTADO FROM TALUNO
-          WHERE COD_ALUNO = 1)
-AND COD_ALUNO <> 1;
+SELECT cod_aluno, nome, cidade, estado
+FROM taluno
+WHERE (cidade, estado) =
+         (SELECT cidade, estado FROM taluno
+          WHERE cod_aluno = 1)
+AND cod_aluno <> 1;
 
-SELECT * FROM TALUNO;
-SELECT * FROM TGESTAO;
+SELECT * FROM taluno;
+SELECT * FROM tgestao;
 
 --Soma toda a gestao, e mostra somente cujo o valor minimo
 --seja maior que o valor medio dos cursos
-SELECT COD_CURSO, Min(PAGAMENTO), Sum(PAGAMENTO),
-       Count(*) QTDE
-FROM TGESTAO
+SELECT cod_curso, Min(pagamento), Sum(pagamento),
+       Count(*) qtde
+FROM tgestao
 WHERE cod_curso > 0
-GROUP BY COD_CURSO
-HAVING Min(PAGAMENTO) >=
-       (SELECT Avg(VALOR) FROM TCURSO)
+GROUP BY cod_curso
+HAVING Min(pagamento) >=
+       (SELECT Avg(valor) FROM tcurso)
 ORDER BY cod_curso;
 
 
 --Soma total de contrato por aluno e mostra somente
 --cujo o menor contrato seja maior que o valor medio de curso
-SELECT COD_ALUNO, Min(TOTAL), Sum(TOTAL)
-FROM TCONTRATO
-GROUP BY COD_ALUNO
-HAVING Min(TOTAL) >
-       (SELECT Avg(VALOR) FROM TCURSO);
+SELECT cod_aluno, Min(total), Sum(total)
+FROM tcontrato
+GROUP BY cod_aluno
+HAVING Min(total) >
+       (SELECT Avg(valor) FROM tcurso);
 
 
 --Todos os cursos de questao na tabela de gestao (vendidos)
-SELECT COD_CURSO, NOME, VALOR
-FROM TCURSO
-WHERE COD_CURSO IN (SELECT COD_CURSO FROM TGESTAO);
+SELECT cod_curso, nome, valor
+FROM tcurso
+WHERE cod_curso IN (SELECT cod_curso FROM tgestao);
 
 
 --Todos os cursos que nao estao na tabela de gestao
 --(Nao vendidos)
-SELECT COD_CURSO, NOME, VALOR
-FROM TCURSO
-WHERE COD_CURSO NOT IN (SELECT COD_CURSO FROM TGESTAO)
-ORDER BY NOME
+SELECT cod_curso, nome, valor
+FROM tcurso
+WHERE cod_curso NOT IN (SELECT cod-curso FROM tgestao)
+ORDER BY nome
 
 
 --Codigo equivalente a subselect
 --(se os valores sao conhecidos)
-SELECT COD_CURSO, NOME, VALOR
-FROM TCURSO WHERE COD_CURSO IN (1,2,3,4);
+SELECT cod_curso, nome, valor
+FROM tcurso WHERE cod_curso IN (1,2,3,4);
 
 --
-SELECT COD_CURSO, NOME, VALOR
-FROM TCURSO
-WHERE COD_CURSO = 1
-OR COD_CURSO = 2
-OR COD_CURSO = 3
-OR COD_CURSO = 4;
+SELECT cod_curso, nome. valor
+FROM tcurso
+WHERE cod_curso = 1
+OR cod_curso = 2
+OR cod_curso = 3
+OR cod_curso = 4;
 
 --Todos cursos que foram vendidos pelo valor padrao
-SELECT * FROM TGESTAO
-WHERE (COD_CURSO, PAGAMENTO) IN
-      (SELECT COD_CURSO, VALOR FROM TCURSO)
+SELECT * FROM tgestao
+WHERE (cod_curso, pagamento) IN
+      (SELECT cod_curso, valor FROM tcurso)
 
 --SubCoonsulta na clausula From
-SELECT GES.COD_CONTRATO, GES.PAGAMENTO,
-       GES.COD_CURSO,
-       CUR.COD_CURSO, CUR.VALOR
-FROM TGESTAO GES,
-     (SELECT COD_CURSO, VALOR
-      FROM TCURSO WHERE VALOR > 500 ) CUR
+SELECT ges.cod_contrato, ges.pagamento,
+       ges.cod_curso,
+       cur.cod_curso, cur.valor
+FROM tgestao ges,
+     (SELECT cod_curso, valor
+      FROM tcurso WHERE valor > 500 ) cur
 
-WHERE CUR.COD_CURSO = GES.COD_CURSO
+WHERE cur.cod_curso = ges.cod_curso
 
 COMMIT;
